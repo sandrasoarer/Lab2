8ad63e46669bd23d6e4d295388f7db32efca33f6
LesnickBear <Lesnickbear@gmail.com>
2018-04-10 21:28:44 +0300

    Factory of Objects added

--- NoteApp/src/main/java/com/bas/Engine.java
+++ NoteApp/src/main/java/com/bas/Engine.java
@@ -9,28 +9,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Engine extends Application {
-    Stage mainFormStage;
-    Stage addFormStage;
-    Stage editFormStage;
+    Stage mainFormStage = new Stage();
+    Stage addFormStage= new Stage();;
+    Stage editFormStage= new Stage();;
     AnchorPane mainPane;
     AnchorPane addPane;
     AnchorPane editPane;
     Engine engine;
     {
-        FXMLLoader loader = new FXMLLoader();
-        try {
-            mainPane = FXMLLoader.load(Engine.class.getResource("./view/MainForm.fxml"));
-            addPane = FXMLLoader.load(Engine.class.getResource("./view/AddForm"));
-            editPane = FXMLLoader.load(Engine.class.getResource("./view/EditForm"));
-            Scene scene = new Scene(mainPane);
-            mainFormStage.setScene(scene);
-            scene = new Scene(addPane);
-            addFormStage.setScene(scene);
-            scene = new Scene(editPane);
-            editFormStage.setScene(scene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
     public static void main(String[] args) {
         Application.launch(Engine.class);
@@ -41,6 +28,20 @@ public class Engine extends Application {
         engine = this;
         this.mainFormStage = primaryStage;
         mainFormStage.setTitle("NotePad");
+        initPanes();
         mainFormStage.show();
+    }
+    public void initPanes()
+    {
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            mainPane = FXMLLoader.load(Engine.class.getResource("view/MainForm.fxml"));
+            addPane = FXMLLoader.load(Engine.class.getResource("./view/AddForm.fxml"));
+            editPane = FXMLLoader.load(Engine.class.getResource("./view/EditForm.fxml"));
+            Scene scene = new Scene(mainPane);
+            mainFormStage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
